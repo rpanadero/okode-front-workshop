853692fbfc70a59f184edcdb2061375f3c102f8b
Rubén Panadero <rpanadero@okode.com>
2021-10-10 20:33:20 +0200

    feat(rxjs): implemented product search on input typing

--- src/app/features/products/pages/product-search/product-search.page.ts
+++ src/app/features/products/pages/product-search/product-search.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
+import { fromEvent, of, Subscription } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 import { Product } from 'src/app/core/models/product.model';
 import { ProductHttpService } from 'src/app/core/services/http/product-http.service';
 
@@ -7,13 +9,40 @@ import { ProductHttpService } from 'src/app/core/services/http/product-http.serv
   templateUrl: './product-search.page.html',
   styleUrls: ['./product-search.page.scss']
 })
-export class ProductSearchPage {
+export class ProductSearchPage implements AfterViewInit, OnDestroy {
 
+  @ViewChild('input')
+  input?: ElementRef;
   products?: Product[];
   searchTerm?: string;
+
+  private inputTypeSubscription?: Subscription;
 
   constructor(
     private productHttpService: ProductHttpService // Use this service to get products
   ) { }
+
+  ngAfterViewInit() {
+    this.bindProductsSearchOnInputType();
+  }
+
+  ngOnDestroy() {
+    this.inputTypeSubscription?.unsubscribe();
+  }
+
+  private bindProductsSearchOnInputType() {
+    this.inputTypeSubscription = fromEvent(this.input?.nativeElement, 'keyup').pipe(
+      switchMap(
+        () =>  {
+          if (!this.searchTerm || this.searchTerm.length < 3) { return of<Product[]>([]); }
+          return this.productHttpService.getProducts(this.searchTerm);
+        }
+      )
+    ).subscribe(products => {
+      this.products = products;
+    }, err => {
+      // TODO: show error
+    });
+  }
 
 }
